707a67ef02baf9567e57b8dbb2c5a5126a8ba2cf
cyberiansarthak <113530791+cyberiansarthak@users.noreply.github.com>
2023-02-24 21:27:04 -0800

    day4

---
+++ Chapter_06/03_In.py
@@ -0,0 +1,7 @@
+# THE USE OF (IN) IN CONDITIONAL STATEMENT
+A=[1,3,4,7,8]
+Input = int(input("Enter the number\n"))
+if(Input in A):
+    print("Number presnt in our list")
+else:
+    print("Not present")    
