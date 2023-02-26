e46036b126c36a3094ee7fdee15121ed27865e99
cyberiansarthak <113530791+cyberiansarthak@users.noreply.github.com>
2023-02-25 19:33:04 -0800

    day5

---
+++ Chapter_07/08_Continue_statement.py
@@ -0,0 +1,6 @@
+# CONTINUE IS USED TO SKIP THE STATEMENT 
+for i in range(10):
+    if(i==5):
+        continue    # it skip the print statement at the stage of 5 and print the statment after that.
+    print(i)
+print("\n")  
---
+++ Chapter_07/09_Pass_statement.py
@@ -0,0 +1,9 @@
+#PASS STATEMENT IS THE NULL STATEMENT IN PYTHON IT INSTRUCT TO "DO NOTHING"
+L =[1,4,8,9]
+for item in L:
+    pass   # without pass statement this for loop throw an error
+
+i=0
+if (i<10):
+    pass
+print( "We pass through the above statement without showing an error")
