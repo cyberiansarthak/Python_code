5f9fcf1cbd9917b56f88bbf5d18211d1673a7865
cyberiansarthak <113530791+cyberiansarthak@users.noreply.github.com>
2023-02-25 19:12:50 -0800

    day5

---
+++ Chapter_07/04_For_loop.py
@@ -0,0 +1,4 @@
+#FOR LOOP IS USED TO ITERATE THROUGH A SEQUENCE LIST,SET TUPPLE AND STRING
+Str= "sarthak khare"
+for String in Str: # IN FOR LOOP WE CREATE A VARIABLE LIKE(String) it iterate through loop using(in)
+    print(String)
---
+++ Chapter_07/05_Range_func.py
@@ -0,0 +1,22 @@
+#RANGE FUNCTION IS USED TO GENERATE A SEQUENCE OF NUMBERS
+for i in range(10):
+    print(i)
+print("\n")    
+#WE CAN ALSO SPECIFY THE START,STOP AND STEP-SIZE
+
+#START
+for i in range(1,50): # IT PRINT THE NUMBER FROM 1-49 WHICH IS ONE LESS THEN 49
+    print(i) 
+print("\n")  
+
+#STEP SIZE
+#PRINT TABLE OF 5
+for i in range(5,55,5):
+    print(i)
+print("\n")  
+
+#BY DEFAULT STEP-SIZE IS ONE
+for i in range(1,10,1):
+    print(i)
+
+    
