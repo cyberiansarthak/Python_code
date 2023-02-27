ab4c39d588c587824329521605f0b116665ba3ee
cyberiansarthak <113530791+cyberiansarthak@users.noreply.github.com>
2023-02-26 18:57:48 -0800

    day6

--- Chapter_08/04_starUsingRecursion.py
+++ Chapter_08/04_starUsingRecursion.py
@@ -1,11 +1,23 @@
 #STAR PATTERN USING RECURSION
-#FUNCTION FOR ROWS
+inp =int(input("enter number of rows:\n"))   
+#FUNCTION FOR Upper triangle
 def printfun(n):
     if( n==0 ):
         return 
     else:
         printfun(n-1)
         print(" * "*n)
-inp =int(input("enter number of rows:\n"))     
+
+printfun(inp) 
+
+#FUNCTION FOR LOWER TRIANGLE
+def printfun(n):
+    if( n==0 ):
+        return 
+    else:
+         print(" * "*n)
+    printfun(n-1)
+       
+
 printfun(inp)
 
