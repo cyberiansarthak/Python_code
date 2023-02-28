621527ad46b03e0ec522081b5c4fc45479ad175d
cyberiansarthak <113530791+cyberiansarthak@users.noreply.github.com>
2023-02-27 22:01:50 -0800

    day7

---
+++ Chapter_09/03_Modes.py
@@ -0,0 +1,8 @@
+# MODES USED IN FILE FUNCTION
+# r ---> open for reading
+# w ---> open for writing
+# a ---> open for appending
+# + ----> open for uploading
+
+# "rb" will open for read in binary mode
+# "rt" will open for read in text mode
---
+++ Chapter_09/04_Write.py
@@ -0,0 +1,3 @@
+# WRITE FUNCTION TO WRITE SOMETHING IN THE FILE
+f =open("another.txt","w")
+f.write("please write this to the file")
