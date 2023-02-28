87906631852b0bc4255903075edbf5f099d8eeed
cyberiansarthak <113530791+cyberiansarthak@users.noreply.github.com>
2023-02-28 11:28:52 -0800

    day8

---
+++ Question.py/07_Try&catch.py
@@ -0,0 +1,5 @@
+List = [1,5,6,7,8]
+
+try : print(List[6])
+except: print("out of range")
+
---
+++ Question.py/08_Os_access.py
@@ -0,0 +1,12 @@
+import os 
+ret =os.access(r"C:\Users\Lenovo\OneDrive\Desktop\python_lab",os.F_OK)
+print ("F_OK - return value %s", ret)
+
+ret1 =os.access(r"C:\Users\Lenovo\OneDrive\Desktop\python_lab",os.R_OK)
+print ("R_OK - return value %s", ret1)
+
+ret2 =os.access(r"C:\Users\Lenovo\OneDrive\Desktop\doremon",os.W_OK)
+print ("W_OK - return value %s", ret2)
+
+ret3 =os.access(r"C:\Users\Lenovo\OneDrive\Desktop\python_lab\Chapter_01\01_hello.py",os.X_OK)
+print ("X_OK - return value %s", ret3)
