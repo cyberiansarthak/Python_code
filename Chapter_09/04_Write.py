8448cc98d1350310a221a2a11e63704ffc4a2592
cyberiansarthak <113530791+cyberiansarthak@users.noreply.github.com>
2023-02-27 22:05:53 -0800

    day7

--- Chapter_09/04_Write.py
+++ Chapter_09/04_Write.py
@@ -1,3 +1,4 @@
 # WRITE FUNCTION TO WRITE SOMETHING IN THE FILE
 f =open("another.txt","w")
 f.write("please write this to the file")
+f.close()
---
+++ Chapter_09/05_Appending.py
@@ -0,0 +1,5 @@
+# WRITE FUNCTION TO Append SOMETHING IN THE FILE
+f =open("another.txt","a")
+f.write("i am appending")
+f.close()
+#IF YOU  RUN THIS APPEND CODE N TIME THEN THERE WILL BE N LINES ADD IN THE FILE
