682ccfc16e907256dcfb6f80f9e96f05ee0462bc
cyberiansarthak <113530791+cyberiansarthak@users.noreply.github.com>
2023-02-28 22:08:33 -0800

    day8

---
+++ Chapter_10/02_Class&Object.py
@@ -0,0 +1,20 @@
+#CLASS 
+#A CLASS IS A BLUEPRINT FOR CREATING OBJECTS
+#syntax
+class PascalConvention_name:     #class creation
+     name ="sarthak"             #class variable
+     def isPrint(self):                  #class method(use camel convention)
+        return print(self.name),print(self.branch)
+
+
+#OBJECT
+#AN OBEJCT IS A INSTANTIATION OF A CLASS .WHEN CLASS IS DEFINED, 
+# A TEMPLATE(INFO) IS DEFINED MEMORY IS ALLOCATED ONLY AFTER OBJECT CREATE.
+#syntax
+Student = PascalConvention_name() #object creation 
+Student.name="Dev"                #instance variable
+Student.branch="icb"              #instance variable
+Student.isPrint()
+
+     
+
