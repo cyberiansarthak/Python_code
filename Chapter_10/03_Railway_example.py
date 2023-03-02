79b7cdf083a42cb2160c5f0897dca8d72a0907a4
cyberiansarthak <113530791+cyberiansarthak@users.noreply.github.com>
2023-03-01 21:42:21 -0800

    day9

---
+++ Chapter_10/03_Railway_example.py
@@ -0,0 +1,18 @@
+#RAILWAY EAXMPLE
+class RailwayTicket:
+    def printTicket(self):
+        print(self.name)
+        print(self.age)
+        print(self.seat_no)
+        print(self.date)
+        print(self.time)
+        print(self.destination)
+
+Passenger_1=RailwayTicket()
+Passenger_1.name="Sarthak"
+Passenger_1.age=18
+Passenger_1.seat_no="78 S9"
+Passenger_1.date="9/02/2023" 
+Passenger_1.time="9:30am" 
+Passenger_1.destination="Vidisha --> Bhopal" 
+Passenger_1.printTicket()   
