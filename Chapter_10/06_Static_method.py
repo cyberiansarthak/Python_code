c0e0b456f660e705ea56d6e0c089756edc6b04ac
cyberiansarthak <113530791+cyberiansarthak@users.noreply.github.com>
2023-03-01 22:29:32 -0800

    day9

---
+++ Chapter_10/06_Static_method.py
@@ -0,0 +1,16 @@
+#SOMETIMES WE DONT'T WANT TO PASS (SELF) INSIDE OUR CLASS METHOD PARAMETER THEN WE USE STATIC METHOD
+
+class StaticClass:
+    @staticmethod               #this annotation is used to represents static method
+    def isStatic_method():
+        print("I am static method")
+
+
+     @staticmethod                    # we can not use self parameter in static method
+    def Static_method(self):                #this line throw an error
+        print("I am static method")
+
+
+StaticClass.isStatic_method()      # now we can use this method without passing any parameter
+StaticClass.isStatic_method(parmeter)    #this line throw an error
+
