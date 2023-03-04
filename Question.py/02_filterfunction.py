f2f4c8fdb78d98b7069e825212ffc83b0f1edd7e
sarthak-io <113530791+sarthak-io@users.noreply.github.com>
2023-03-04 20:51:16 +0530

    Day12

--- Question.py/02_filterfunction.py
+++ Question.py/02_filterfunction.py
@@ -1,1 +1,14 @@
 #filter function
+#It return a iterator where the items are filtered through a function to test if the item is acceptd or not
+name = ["sarthak","dev","piyush","udit"]
+
+def Cloudburst(name):
+  if name=="udit":
+    return False
+  else:
+    return True
+
+List = filter(Cloudburst, name)
+
+for x in List:
+  print(x)
