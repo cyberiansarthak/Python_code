e0a7a308ffa519728fdaf612bba7791b2ad2b397
sarthak-io <113530791+sarthak-io@users.noreply.github.com>
2023-03-21 12:20:18 +0530

    update

--- Lab 4/readfile.py
+++
@@ -1,2 +0,0 @@
-f = open("Lab 4/demofile.txt", "r")
-print(f.read())
--- Lab 4/write.py
+++
@@ -1,6 +0,0 @@
-f = open("Lab 4/demofile.txt", "a")
-f.write("Now the file has more content!\n")
-f.close()
-
-f = open("Lab 4/demofile.txt", "r")
-print(f.read())
